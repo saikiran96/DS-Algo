7f78c83bba46bf4689193d0ae872d955c0192eda
saikiran96 <satyasaikiran96@gmail.com>
2025-12-18 22:07:15 +0530

    Commit on 18-11 :

--- Leetcode/Array-String/merge-strings-alternately.js
+++ Leetcode/Array-String/merge-strings-alternately.js
@@ -8,16 +8,27 @@ var mergeAlternately = function(word1, word2) {
     let merged = [];
     let i = 0;
     let j = 0;
+    const len1 = word1.length;
+    const len2 = word2.length;
 
-    while (i < word1.length || j < word2.length) {
-        if (i < word1.length) {
-            merged.push(word1[i]);
-            i++;
-        }
-        if (j < word2.length) {
-            merged.push(word2[j]);
-            j++;
-        }
+    // Alternate merging as long as both strings have characters
+    while (i < len1 && j < len2) {
+        merged.push(word1[i]);
+        merged.push(word2[j]);
+        i++;
+        j++;
+    }
+
+    // Append any remaining characters from word1
+    while (i < len1) {
+        merged.push(word1[i]);
+        i++;
+    }
+
+    // Append any remaining characters from word2
+    while (j < len2) {
+        merged.push(word2[j]);
+        j++;
     }
 
     return merged.join('');
