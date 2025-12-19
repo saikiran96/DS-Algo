0024646b34448463570be313c6d9cefb0674ccb2
saikiran96 <satyasaikiran96@gmail.com>
2025-12-19 21:47:34 +0530

    Commit on 19-11 :

--- Leetcode/Array-String/kids-with-the-greatest-number-of-candies.js
+++ Leetcode/Array-String/kids-with-the-greatest-number-of-candies.js
@@ -5,9 +5,17 @@
  * @return {boolean[]}
  */
 var kidsWithCandies = function(candies, extraCandies) {
-    const maxCandies = Math.max(...candies);
-    const result = [];
+    let maxCandies = 0;
+    for (let i = 0; i < candies.length; i++) {
+        if (candies[i] > maxCandies) {
+            maxCandies = candies[i];
+        }
+    }
 
+    // A more concise way to find max:
+    // const maxCandies = Math.max(...candies);
+
+    const result = [];
     for (let i = 0; i < candies.length; i++) {
         result.push(candies[i] + extraCandies >= maxCandies);
     }
