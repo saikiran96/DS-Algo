dff2b38abc3f01021e357b62a03e67448a397791
saikiran96 <satyasaikiran96@gmail.com>
2025-12-22 23:42:56 +0530

    Commit on 22-11 :

--- Leetcode/Two-Pointers/container-with-most-water.js
+++ Leetcode/Two-Pointers/container-with-most-water.js
@@ -9,17 +9,11 @@ var maxArea = function(height) {
     let right = height.length - 1;
 
     while (left < right) {
-        // Calculate the current area formed by lines at 'left' and 'right'
         const currentHeight = Math.min(height[left], height[right]);
-        const width = right - left;
-        const currentArea = currentHeight * width;
-
-        // Update the maximum area found so far
+        const currentWidth = right - left;
+        const currentArea = currentHeight * currentWidth;
         maxArea = Math.max(maxArea, currentArea);
 
-        // Move the pointer pointing to the shorter line inward
-        // This is because moving the shorter line has the potential to increase the min(height)
-        // Moving the taller line will definitely decrease the width and might not increase the min(height)
         if (height[left] < height[right]) {
             left++;
         } else {
