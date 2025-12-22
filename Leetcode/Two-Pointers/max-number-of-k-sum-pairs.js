2eb513950e64c82bd9a5fd9ed05fc7a19eafea62
saikiran96 <satyasaikiran96@gmail.com>
2025-12-22 23:43:49 +0530

    Commit on 22-11 :

--- Leetcode/Two-Pointers/max-number-of-k-sum-pairs.js
+++ Leetcode/Two-Pointers/max-number-of-k-sum-pairs.js
@@ -5,17 +5,19 @@
  * @return {number}
  */
 var maxOperations = function(nums, k) {
-    const freqMap = new Map();
     let operations = 0;
+    const counts = new Map();
 
     for (const num of nums) {
         const complement = k - num;
-
-        if (freqMap.has(complement) && freqMap.get(complement) > 0) {
+        if (counts.has(complement) && counts.get(complement) > 0) {
+            // Found a pair!
             operations++;
-            freqMap.set(complement, freqMap.get(complement) - 1);
+            counts.set(complement, counts.get(complement) - 1);
         } else {
-            freqMap.set(num, (freqMap.get(num) || 0) + 1);
+            // No complement found, or complement count is 0.
+            // Store or increment the current number's count.
+            counts.set(num, (counts.get(num) || 0) + 1);
         }
     }
 
