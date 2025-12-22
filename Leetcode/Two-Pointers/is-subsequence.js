8e49ff6be5cf181af304920d01bc4ecdd7162ecf
saikiran96 <satyasaikiran96@gmail.com>
2025-12-22 23:01:19 +0530

    Commit on 22-11 :

---
+++ Leetcode/Two-Pointers/is-subsequence.js
@@ -0,0 +1,19 @@
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let sPointer = 0;
+    let tPointer = 0;
+
+    while (sPointer < s.length && tPointer < t.length) {
+        if (s[sPointer] === t[tPointer]) {
+            sPointer++;
+        }
+        tPointer++;
+    }
+
+    return sPointer === s.length;
+};
