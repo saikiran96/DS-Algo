fe7d4bc5089a56dfbb38e354721739a54e5afc9e
saikiran96 <satyasaikiran96@gmail.com>
2025-12-22 23:06:30 +0530

    Commit on 22-11 :

--- Leetcode/Two-Pointers/move-zeroes.js
+++ Leetcode/Two-Pointers/move-zeroes.js
@@ -4,16 +4,25 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let writePointer = 0;
+    let writePointer = 0; // Pointer to place the next non-zero element
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== 0) {
-            nums[writePointer] = nums[i];
+    // Iterate through the array with 'readPointer'
+    for (let readPointer = 0; readPointer < nums.length; readPointer++) {
+        // If the element at 'readPointer' is non-zero
+        if (nums[readPointer] !== 0) {
+            // If 'readPointer' and 'writePointer' are different,
+            // it means we found a non-zero element at 'readPointer' and
+            // there's either a zero or another non-zero element (which is already in its correct place)
+            // at 'writePointer'. To maintain relative order and move zeros, we swap.
+            // The check 'readPointer !== writePointer' prevents unnecessary swaps
+            // when the array segment before 'readPointer' has no zeros.
+            if (readPointer !== writePointer) {
+                let temp = nums[readPointer];
+                nums[readPointer] = nums[writePointer];
+                nums[writePointer] = temp;
+            }
+            // Increment 'writePointer' to prepare for the next non-zero element
             writePointer++;
         }
-    }
-
-    for (let i = writePointer; i < nums.length; i++) {
-        nums[i] = 0;
     }
 };
