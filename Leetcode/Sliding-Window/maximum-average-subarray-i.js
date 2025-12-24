ed70a4f2f838e21de4802498779cac68e02e8aea
saikiran96 <satyasaikiran96@gmail.com>
2025-12-24 20:57:14 +0530

    Commit on 24-11 :

--- Leetcode/Sliding-Window/maximum-average-subarray-i.js
+++ Leetcode/Sliding-Window/maximum-average-subarray-i.js
@@ -5,26 +5,24 @@
  * @return {number}
  */
 var findMaxAverage = function(nums, k) {
+    // Calculate the sum of the first k elements
     let currentSum = 0;
-
-    // Calculate the sum of the first 'k' elements
     for (let i = 0; i < k; i++) {
         currentSum += nums[i];
     }
 
     let maxSum = currentSum;
 
-    // Slide the window
+    // Slide the window through the rest of the array
     for (let i = k; i < nums.length; i++) {
-        // Subtract the element leaving the window
-        // Add the new element entering the window
-        currentSum = currentSum - nums[i - k] + nums[i];
+        // Add the new element entering the window (nums[i])
+        // Subtract the element leaving the window (nums[i - k])
+        currentSum = currentSum + nums[i] - nums[i - k];
         
-        // Update maxSum if currentSum is greater
-        if (currentSum > maxSum) {
-            maxSum = currentSum;
-        }
+        // Update maxSum if the current window sum is greater
+        maxSum = Math.max(maxSum, currentSum);
     }
 
+    // The maximum average is the maxSum divided by k
     return maxSum / k;
 };
