f0198e46228547a53730e2e5bfacd1ed3fe48cba
saikiran96 <satyasaikiran96@gmail.com>
2026-01-18 16:14:41 +0530

    Commit on 18-0 :

---
+++ LeetCode150/Prefix-Sum/find-the-highest-altitude.js
@@ -0,0 +1,18 @@
+
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function(gain) {
+    let currentAltitude = 0;
+    let maxAltitude = 0;
+
+    for (let i = 0; i < gain.length; i++) {
+        currentAltitude += gain[i];
+        if (currentAltitude > maxAltitude) {
+            maxAltitude = currentAltitude;
+        }
+    }
+
+    return maxAltitude;
+};
