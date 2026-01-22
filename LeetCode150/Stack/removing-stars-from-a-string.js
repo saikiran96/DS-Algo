42c51f2bb4508fc0e289c663e2439dd529861059
saikiran96 <satyasaikiran96@gmail.com>
2026-01-22 21:58:04 +0530

    Commit on 22-0 :

---
+++ LeetCode150/Stack/removing-stars-from-a-string.js
@@ -0,0 +1,20 @@
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function(s) {
+    const stack = [];
+
+    for (const char of s) {
+        if (char === '*') {
+            if (stack.length > 0) {
+                stack.pop();
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.join('');
+};
